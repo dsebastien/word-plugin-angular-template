e5f791d742bccbbfe66f4c529d9e3f378251c950
Sebastien Dubois <seb@dsebastien.net>
2020-11-15 15:59:01 +0100

    Initial commit

---
+++ src/taskpane/taskpane.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+import "zone.js"; // Required for Angular
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import AppModule from "./app/app.module";
+/* global console, document, Office */
+
+Office.initialize = (_reason) => {
+  let sideLoadMessageElement = document.getElementById("sideload-msg");
+  if(sideLoadMessageElement) {
+    sideLoadMessageElement.style.display = "none";
+  }
+
+  // Bootstrap the app
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(error => console.error(error));
+};
